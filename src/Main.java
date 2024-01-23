10634ba00e1e673a82401e173950ee8140f5c000
luishermes <luishenriquefrance@gmail.com>
2024-01-23 13:34:43 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // int, double, float, char, byte, short, long, boolean
+        // Tipos primitivos.
+        int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
+
+
+
+        System.out.println("A idade é " + idade + " anos ");
+
+
+
+    }
+}
